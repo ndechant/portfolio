2cb2eff4085020e55f233b0f8798aeed2d9655bf
Nick T. DeChant <nick.dechant@utexas.edu>
2012-12-18 19:43:46 -0600

    initial commit: portfolio

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Portfolio::Application.config.secret_token = '216e58ee721efb0299774f7f6753d3e4f3aa314a701c3a9dc87138c51edf42a5b920bced15fd98ac9b3c7a586bfd5851bc55c0aa904521ec2f0c8bfded7ed339'
